06d36af6eed2460b5b866e39b470faab77991fd8
VivekKumarbehra <vivekbehera0807@gmail.com>
2026-07-12 22:49:11 +0530

    Insertion LinkList

---
+++ src/LinkList/InsertionLinkList.java
@@ -0,0 +1,26 @@
+package LinkList;
+
+public class InsertionLinkList {
+    public static void main(String[] args) {
+
+    }
+
+    public class ListNode {
+    int val;
+      ListNode next;
+      ListNode(int x) {
+          val = x;
+          next = null;
+     }
+  }
+    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+      ListNode temp1= headA;
+      ListNode temp2 = headB;
+      while(temp1 != temp2){
+          temp1 = (temp1 == null) ? headB : temp1.next;
+          temp2=(temp2 == null) ? headA : temp2.next;
+      }
+      return temp2;
+
+    }
+}
